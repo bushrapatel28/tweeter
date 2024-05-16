6b39505754460acb9245dfc5da24837a123c3b32
Bushra Patel <bpatel60@yahoo.com>
2024-05-16 19:31:30 -0400

    Set an event handler for keyboard inputs in #tweet-text (textarea). Traverse the DOM to locate the .counter class (output) and update the counter value to show remaining character limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  const textArea = $('#tweet-text');
+
+  textArea.on('input', function(event) {
+    const length = $(this).val().length;        //Same as (textArea.val().length);
+
+    //Traverse up the DOM tree to get the parent 'form' then locate child 'class="button-counter"' then look for its child 'class="counter"'
+    const counterClass = $(this).parents('form').children('.button-counter').children('.counter');
+    const counter = 140 - length;
+    //counterClass updating with the value.
+    counterClass.val(counter);
+
+    //Add a new class to .counter for styling negative counter values.
+    if (counter < 0) {
+      $(counterClass).addClass("negative");
+    } else {
+      $(counterClass).removeClass("negative");
+    }
+  });
+});
