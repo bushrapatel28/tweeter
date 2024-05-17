ba4ba8be0f26d0e837652a4acd7efea7caa8dc0b
Bushra Patel <bpatel60@yahoo.com>
2024-05-17 16:53:44 -0400

    Set hover events to handle icon color change and enable box-shadow

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,26 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const footerIcons = $('#footer-icons');
+  const article = $('.tweets');
+
+  footerIcons.on('mouseover', function(event) {
+    const icon = event.target;
+    $(icon).addClass("highlight");
+
+    $(icon).on('mouseleave', function(event) {
+      $(this).removeClass("highlight");
+    });
+  });
+
+  article.on('mouseover', function(event) {
+    const box = event.currentTarget;
+    $(box).addClass("shadow");
+
+    $(box).on('mouseleave', function(event) {
+      $(this).removeClass("shadow");
+    });
+  });
+
+});
