28ca28af20ae8a57b3f60369fa2727b4731ba150
Bushra Patel <bpatel60@yahoo.com>
2024-05-21 22:13:33 -0400

    Functions (using jQuery) to create the Tweet Elements and Render them into the browser page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,25 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const footerIcons = $('#footer-icons');
-  const article = $('.tweets');
 
-  footerIcons.on('mouseover', function(event) {
-    const icon = event.target;
-    $(icon).addClass("highlight");
+  //Create new tweet elements
+  const createTweetElement = function(tweetObj) {
+    const userData = tweetObj.user;
+    const tweetContent = tweetObj.content;
+    const date = tweetObj.created_at;
+    
+    //Create article element with class name of 'tweets'
+    const $tweet = $(`<article class="tweets">
+                        <header>
+                          <section class="user">
+                            <img src="${userData.avatars}" alt="Avatar">
+                            <h3>${userData.name}</h3>
+                          </section>
+                          <aside>${userData.handle}</aside>
+                        </header>
+                        <section class="tweet-content">${tweetContent.text}</section>
+                        <footer>
+                          <time>${date}</time>
+                          <aside class="footer-icons">
+                            <i class="fa-solid fa-flag"></i>
+                            <i class="fa-solid fa-retweet"></i>
+                            <i class="fa-solid fa-heart"></i>
+                          </aside>
+                        </footer>
+                      </article>`);
+    return $tweet;
+  };
+  
+  const renderTweets = function(tweetObjArr) {
+    for(const element of tweetObjArr) {
+      //Tweet Data returned by the function
+      const $tweet = createTweetElement(element);
+      
+      //Add it to the page so we can make sure it's got all the right elements, classes, etc.
+      $('#tweets-container').append($tweet);
+    }
+  }
 
-    $(icon).on('mouseleave', function(event) {
-      $(this).removeClass("highlight");
-    });
-  });
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
 
-  article.on('mouseover', function(event) {
-    const box = event.currentTarget;
-    $(box).addClass("shadow");
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-    $(box).on('mouseleave', function(event) {
-      $(this).removeClass("shadow");
-    });
-  });
-
+  renderTweets(data);
 });
