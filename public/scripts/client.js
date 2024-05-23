ccd641a6b86ffa5004d87f542ff334424339ca06
Bushra Patel <bpatel60@yahoo.com>
2024-05-22 22:22:55 -0400

    Add loadtweets function to make a jQuery AJAX GET request to fetch the data in JSON format and render the server response into the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,31 +3,33 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+$(document).ready(function () {
+
   //Form 'submit' Event Handler
   $('form').on('submit', function(event) {
-    alert( "Handler for `submit` called." );
     //Prevent the default form submission behaviour
     event.preventDefault();
     //Convert (serialize) the form data into jQuery String
     const serData = $(this).serialize();
 
-    //jQuery AJAX Post Request (xhr)
+    //jQuery AJAX Post Request (xhr); same as $.post("/tweets", serData);
     $.ajax({
       type: "POST",
       url: "/tweets",
       data: serData,       //serialized data sent to server
-    })
+    });
+    // //Load the tweet as soon as it is posted
+    // loadTweets();
   });
 
   //Create new tweet elements
   const createTweetElement = function(tweetObj) {
     const userData = tweetObj.user;
     const tweetContent = tweetObj.content;
-    const date = tweetObj.created_at;
-    
+    const timeAgo = timeago.format(tweetObj.created_at);      //timeago.format() to display the time passed since a Tweet
+
     //Create article element with class name of 'tweets'
-    let $tweet = $(`<article class="tweets">
+    const $tweet = $(`<article class="tweets">
                         <header>
                           <section class="user">
                             <img src="${userData.avatars}" alt="Avatar">
@@ -37,7 +39,7 @@ $(document).ready(function() {
                         </header>
                         <section class="tweet-content">${tweetContent.text}</section>
                         <footer>
-                          <time>${date}</time>
+                          <time>${timeAgo}</time>
                           <aside class="footer-icons">
                             <i class="fa-solid fa-flag"></i>
                             <i class="fa-solid fa-retweet"></i>
@@ -45,46 +47,34 @@ $(document).ready(function() {
                           </aside>
                         </footer>
                       </article>`);
-
     return $tweet;
   };
-  
-  //Calls createTweetElement for each tweet and takes return value and appends it to the tweets container
+
+  //Calls createTweetElement for each tweet and takes its return value then appends the value to the tweets container
   const renderTweets = function(tweetObjArr) {
-    for(const element of tweetObjArr) {
+    for (const element of tweetObjArr) {
       //Tweet Data returned by the function
       const $tweet = createTweetElement(element);
-      
+
       //Add it to the page so we can make sure it's got all the right elements, classes, etc.
       $('#tweets-container').append($tweet);
     }
   }
 
-  //(Temp) Hard coded data (Taken from initial-tweets.json)
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
+  //Makes a jQuery Ajax Get request to load the tweets as JSON
+  const loadTweets = function() {
+    //jQuery AJAX Get Request (xhr); same as $.get("/tweets", function(response) { renderTweets(response); });
+    $.ajax({
+      url: '/tweets',
+      type: 'GET',
+      dataType: 'json',
+      //Success Callback to call renderTweets by passing the response from the AJAX request``
+      success: function(response) {
+        //Render the JSON data into the page
+        renderTweets(response);
+      }
+    })
+  }
+  //Load tweets upon page load
+  loadTweets();
 });
