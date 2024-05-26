6544f7bdf0afd57e5fe5c933821340f72bc7ada9
Bushra Patel <bpatel60@yahoo.com>
2024-05-25 22:33:27 -0400

    Add comments to describe code blocks functions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 
     //Traverse up the DOM tree to get the parent 'form' then locate child 'class="button-counter"' then look for its child 'class="counter"'
     const counterClass = $(this).parents('form').children('.button-counter').children('.counter');
+    //Counter decreases for each char as it is added to the textarea input
     const counter = 140 - length;
     //counterClass updating with the value.
     counterClass.val(counter);
